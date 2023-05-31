fd1feecf5d0c6800b57e3442f2a4a53b2fa968a2
snehageorge25 <esssneha@gmail.com>
2023-05-31 22:53:29 +0530

    Color palatte changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -30,10 +30,6 @@ const Contact = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      <h5>
-        I do receive your messages and will respond asap if the valid email is
-        provided :)
-      </h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact__options">
